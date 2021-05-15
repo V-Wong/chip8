99d22a064e2c205bc3c8cb62faa05512d8323092
Vincent Wong <vincent@vwong.dev>
2021-05-16 00:00:46 +1000

    Add fetch() implementation

--- src/Emulator/Emulator.cpp
+++ src/Emulator/Emulator.cpp
@@ -1,1 +1,11 @@
 #include "Emulator.h"
+
+uint16_t joinBytes(uint8_t msb, uint8_t lsb) {
+    return ((uint16_t)msb << 8) | lsb;
+}
+
+uint16_t Emulator::fetch(void) {
+    uint16_t instruction = joinBytes(memory.getByte(pc), memory.getByte(pc + 1));
+    pc += 2;
+    return instruction;
+}
