7b0adba767286c991d3dbda372a99b9ff0230612
Vincent Wong <vincent@vwong.dev>
2021-05-16 00:02:34 +1000

    Add run() implementation

--- src/Emulator/Emulator.cpp
+++ src/Emulator/Emulator.cpp
@@ -4,6 +4,13 @@ uint16_t joinBytes(uint8_t msb, uint8_t lsb) {
     return ((uint16_t)msb << 8) | lsb;
 }
 
+void Emulator::run(void) {
+    while (true) {
+        uint16_t instruction = fetch();
+        decodeEexcute(instruction);
+    }
+}
+
 uint16_t Emulator::fetch(void) {
     uint16_t instruction = joinBytes(memory.getByte(pc), memory.getByte(pc + 1));
     pc += 2;
