8e70521ae179812d8eea9be47c5eb21087542978
Vincent Wong <vincent@vwong.dev>
2021-05-18 22:56:51 +1000

    Add push() and pop() implementations

--- src/Stack/Stack.h
+++ src/Stack/Stack.h
@@ -2,13 +2,16 @@
 #define CHIP8_STACK_H
 
 #include <cstdint>
+#include <vector>
 
 
 class Stack {
 public:
-    Stack() = default;
+    Stack(): stack() {};
     void push(std::uint16_t value);
     std::uint16_t pop(void);
+private:
+    std::vector<std::uint16_t> stack;
 };
 
 
