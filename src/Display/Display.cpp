a1de142ee7d4f4ed828a78dab77225213076914c
Vincent Wong <vincent@vwong.dev>
2021-05-19 20:25:34 +1000

    Add draw() implementation

--- src/Display/Display.cpp
+++ src/Display/Display.cpp
@@ -1,1 +1,5 @@
 #include "Display.h"
+
+void Display::draw(int x, int y) {
+    pixels.at(x).at(y) = !pixels.at(x).at(y);
+}
--- src/Display/Display.h
+++ src/Display/Display.h
@@ -1,10 +1,25 @@
 #ifndef CHIP8_DISPLAY_H
 #define CHIP8_DISPLAY_H
 
+#include <memory>
+#include <vector>
+#include "SDL.h"
+
+namespace DisplaySpecs {
+    constexpr int PIXEL_WIDTH = 64;
+    constexpr int PIXEL_HEIGHT = 32;
+}
+
 
 class Display {
 public:
+    Display(): pixels(
+        DisplaySpecs::PIXEL_WIDTH, 
+        std::vector<bool>(DisplaySpecs::PIXEL_HEIGHT)
+    ) {};
     void draw(int x, int y);
+private:
+    std::vector<std::vector<bool>> pixels;
 };
 
 
