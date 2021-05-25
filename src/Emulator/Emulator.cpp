61d5f1083ab8a6b848a5ecbfbc85be1f24a0f8c8
Vincent Wong <vincent@vwong.dev>
2021-05-25 17:41:54 +1000

    Add DecodedInstruction data class

--- src/Emulator/Emulator.cpp
+++ src/Emulator/Emulator.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <stdlib.h>
 
+#include "DecodedInstruction.h"
+
 uint16_t joinBytes(uint8_t msb, uint8_t lsb) {
     return ((uint16_t)msb << 8) | lsb;
 }
@@ -26,107 +28,103 @@ uint16_t Emulator::fetch(void) {
 }
 
 void Emulator::decodeExecute(uint16_t instruction) {
-    uint8_t type = instruction >> 12;
-    uint8_t x = (instruction >> 8) & 0xf;
-    uint8_t y = (instruction >> 4) & 0xf;
-    uint8_t n = instruction & 0xf;
-    uint8_t nn = instruction & 0xff;
-    uint16_t nnn = instruction & 0xfff;
+    DecodedInstruction d{instruction};
 
-    switch(type) {
+    switch (d.type) {
         case 0:
-            if (nnn == 0xE0) {
+            if (d.nnn == 0xE0) {
                 for (int i = 0; i < DisplaySpecs::PIXEL_WIDTH; i++) {
                     for (int j = 0; j < DisplaySpecs::PIXEL_HEIGHT; j++) {
-                        display.unset(x, y);
+                        display.unset(d.x, d.y);
                     }
                 }
             }
-            if (nnn == 0xEE)
+            if (d.nnn == 0xEE)
                 pc = stack.pop();
             break;
         case 1: 
-            pc = nnn - 2;
+            pc = d.nnn - 2;
             break;
         case 2:
             stack.push(pc);
-            pc = nnn;
+            pc = d.nnn;
             break;
         case 3:
-            if (registers[x] == nn) pc += 1;
+            if (registers[d.x] == d.nn) pc += 1;
             break;
         case 4:
-            if (registers[x] != nn) pc += 1;
+            if (registers[d.x] != d.nn) pc += 1;
             break;
         case 5:
-            if (n == 0 && registers[x] == registers[y]) pc += 1;
+            if (d.n == 0 && registers[d.x] == registers[d.y]) pc += 1;
             break;
         case 6:
-            registers[x] = nn;
+            registers[d.x] = d.nn;
             break;
         case 7:
-            registers[x] += nn;
+            registers[d.x] += d.nn;
             break;
         case 8:
-            if (n == 0) registers[x] = registers[y];
-            if (n == 1) registers[x] |= registers[y];
-            if (n == 2) registers[x] &= registers[y];
-            if (n == 3) registers[x] ^= registers[y];
-            if (n == 4) {
-                int val = registers[x];
-                if (val + registers[y] > 255) registers[15] = 1;
+            if (d.n == 0) registers[d.x] = registers[d.y];
+            if (d.n == 1) registers[d.x] |= registers[d.y];
+            if (d.n == 2) registers[d.x] &= registers[d.y];
+            if (d.n == 3) registers[d.x] ^= registers[d.y];
+            if (d.n == 4) {
+                int val = registers[d.x];
+                if (val + registers[d.y] > 255) registers[15] = 1;
                 else registers[15] = 0;
 
-                registers[x] += registers[y];
+                registers[d.x] += registers[d.y];
             }
-            if (n == 5) {
-                int val = registers[x];
-                if (val > registers[y]) registers[15] = 1;
+            if (d.n == 5) {
+                int val = registers[d.x];
+                if (val > registers[d.y]) registers[15] = 1;
                 else registers[15] = 0;
 
-                registers[x] -= registers[y];
+                registers[d.x] -= registers[d.y];
             }
-            if (n == 7) {
-                int val = registers[x];
-                if (val > registers[y]) registers[15] = 1;
+            if (d.n == 7) {
+                int val = registers[d.x];
+                if (val > registers[d.y]) registers[15] = 1;
                 else registers[15] = 0;
 
-                registers[x] = registers[y] - registers[x];
+                registers[d.x] = registers[d.y] - registers[d.x];
             }
-            if (n == 6) {
-                registers[x] = registers[y];
-                if (registers[x] & 1 == 1) registers[15] = 0;
+            if (d.n == 6) {
+                registers[d.x] = registers[d.y];
+                if (registers[d.x] & 1 == 1) registers[15] = 0;
                 else registers[15] = 1;
-                registers[x] >>= 1;
+                registers[d.x] >>= 1;
             }
-            if (n == 0xE) {
-                registers[x] = registers[y];
-                if ((registers[x] >> 15) & 1 == 1) registers[15] = 0;
+            if (d.n == 0xE) {
+                registers[d.x] = registers[d.y];
+                if ((registers[d.x] >> 15) & 1 == 1) registers[15] = 0;
                 else registers[15] = 1;
-                registers[x] <<= 1;
+                registers[d.x] <<= 1;
             }
+            break;
         case 9:
-            if (n == 0 && registers[x] != registers[y]) pc += 1;
+            if (d.n == 0 && registers[d.x] != registers[d.y]) pc += 1;
             break;
         case 0xA:
-            index = nnn;
+            index = d.nnn;
             break;
         case 0xB:
-            pc = nnn + registers[0];
+            pc = d.nnn + registers[0];
             break;
         case 0xC:
-            registers[x] = (rand() % 0xffff) & nn;
+            registers[d.x] = (rand() % 0xffff) & d.nn;
             break;
         case 0xD:
         {
-            uint8_t xCoordinate = registers[x] % 64;
-            uint8_t yCoordinate = registers[y] % 32;
+            uint8_t xCoordinate = registers[d.x] % 64;
+            uint8_t yCoordinate = registers[d.y] % 32;
             registers[15] = 0;
 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < d.n; i++) {
                 uint8_t spriteData = memory.getByte(index + i);
 
-                xCoordinate = registers[x] % 64;
+                xCoordinate = registers[d.x] % 64;
                 for (int j = 0; j < 8; j++) {
                     bool bitSet = (spriteData >> (8 - j - 1)) & 1;
 
@@ -154,48 +152,47 @@ void Emulator::decodeExecute(uint16_t instruction) {
             break;
         }
         case 0xE:
-            if (nn == 0x9E)
-                if (keysPressed[registers[x]])
+            if (d.nn == 0x9E)
+                if (keysPressed[registers[d.x]])
                     pc += 2;
             
-            if (nn == 0xA1)
-                if (!keysPressed[registers[x]])
+            if (d.nn == 0xA1)
+                if (!keysPressed[registers[d.x]])
                     pc += 2;
             
             break;
         case 0xF:
-            if (nn == 0x7)
-                registers[x] = delayTimer;
-            if (nn == 0x15)
-                delayTimer = registers[x];
-            if (nn == 0x18)
-                soundTimer = registers[x];
-            if (nn = 0x1E)
-                index += registers[x];
-            if (nn == 0xA) {
+            if (d.nn == 0x7)
+                registers[d.x] = delayTimer;
+            if (d.nn == 0x15)
+                delayTimer = registers[d.x];
+            if (d.nn == 0x18)
+                soundTimer = registers[d.x];
+            if (d.nn = 0x1E)
+                index += registers[d.x];
+            if (d.nn == 0xA) {
                 if (isBlocked) {
                     for (int i = 0; i < 16; i++) {
                         if (keysPressed[i]) {
-                            registers[x] = i;
+                            registers[d.x] = i;
                         }
                     }
                 } else {
                     isBlocked = true;
                 }
             }
-            if (nn == 0x29)
-                index = registers[x] & 0xF;
-            if (nn == 0x33) {
-                std::cout << "test";
-                registers[index] = (registers[x] / 100) % 10;
-                registers[index + 1] = (registers[x] / 10) % 10;
-                registers[index] = registers[x] % 10;
+            if (d.nn == 0x29)
+                index = registers[d.x] & 0xF;
+            if (d.nn == 0x33) {
+                registers[index] = (registers[d.x] / 100) % 10;
+                registers[index + 1] = (registers[d.x] / 10) % 10;
+                registers[index] = registers[d.x] % 10;
             }
-            if (nn = 0x55)
-                for (int i = 0; i < x; i++)
+            if (d.nn = 0x55)
+                for (int i = 0; i < d.x; i++)
                     memory.writeByte(index + i, registers[i]);
-            if (nn == 0x66)
-                for (int i = 0; i < x; i++)
+            if (d.nn == 0x66)
+                for (int i = 0; i < d.x; i++)
                     registers[i] = memory.getByte(index + i);
             break;
     }
