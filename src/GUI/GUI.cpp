f4c4d2c5c51affb54af6014569f851cf7a71d246
Vincent Wong <vincent@vwong.dev>
2021-06-08 23:40:36 +1000

    Add type inference to GUI class

--- src/GUI/GUI.cpp
+++ src/GUI/GUI.cpp
@@ -4,10 +4,10 @@
 
 #include "../Display/Display.hpp"
 
-constexpr int PIXEL_SIZE = 20;
+auto constexpr PIXEL_SIZE = 20;
 
 
-SDL_Window* GUI::createWindow(void) {
+auto GUI::createWindow(void) -> SDL_Window* {
     return SDL_CreateWindow(
         "chip8", 
         100, 100, 
@@ -16,20 +16,20 @@ SDL_Window* GUI::createWindow(void) {
     );
 }
 
-SDL_Renderer* GUI::createRenderer(void) {
+auto GUI::createRenderer(void) -> SDL_Renderer* {
     return SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 }
 
-void GUI::render(void) {
+auto GUI::render(void) -> void {
     SDL_RenderPresent(renderer);
 }
 
-void GUI::pumpEvents(void) {
+auto GUI::pumpEvents(void) -> void {
     SDL_PumpEvents();
 }
 
-void GUI::drawPixel(int x, int y, int red, int green, int blue) {
-    SDL_Rect r;
+auto GUI::drawPixel(int x, int y, int red, int green, int blue) -> void {
+    auto r = SDL_Rect{};
     r.x = x * PIXEL_SIZE;
     r.y = y * PIXEL_SIZE;
     r.w = r.h = PIXEL_SIZE;
@@ -38,12 +38,12 @@ void GUI::drawPixel(int x, int y, int red, int green, int blue) {
     SDL_RenderFillRect(renderer, &r);
 }
 
-bool GUI::hasExit(void) {
-    SDL_Event ev;
+auto GUI::hasExit(void) -> bool {
+    auto ev = SDL_Event{};
     return (SDL_PollEvent(&ev) != 0 && ev.type == SDL_QUIT) 
             || (isKeyPressed(SDL_SCANCODE_ESCAPE));
 }
 
-void GUI::closeWindow(void) {
+auto GUI::closeWindow(void) -> void {
     SDL_DestroyWindow(window);
 }
