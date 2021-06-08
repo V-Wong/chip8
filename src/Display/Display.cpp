5b2dbde3394830db170d10b26fa7041593aedd0b
Vincent Wong <vincent@vwong.dev>
2021-06-08 23:41:40 +1000

    Add type inference to Display class

--- src/Display/Display.cpp
+++ src/Display/Display.cpp
@@ -1,17 +1,17 @@
 #include "Display.hpp"
 
-void Display::set(uint8_t x, uint8_t y) {
+auto Display::set(uint8_t x, uint8_t y) -> void {
     pixels.at(x).at(y) = true;
 }
 
-void Display::unset(uint8_t x, uint8_t y) {
+auto Display::unset(uint8_t x, uint8_t y) -> void {
     pixels.at(x).at(y) = false;
 }
 
-void Display::flip(uint8_t x, uint8_t y) {
+auto Display::flip(uint8_t x, uint8_t y) -> void {
     pixels.at(x).at(y) = !pixels.at(x).at(y);
 }
 
-bool Display::getPixel(uint8_t x, uint8_t y) {
+auto Display::getPixel(uint8_t x, uint8_t y) -> bool {
     return pixels.at(x).at(y);
 }
